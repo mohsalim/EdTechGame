d014d4e6bed666f3fbbceda4d5dce240a3a3b4e6
Mohammad Hassan Salim <mo_h_salim@hotmail.com>
2018-03-09 15:03:36 -0500

    Start button trigger to fade and start dialogue

---
+++ EdTechGame/Assets/StartGameTrigger.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGameTrigger : MonoBehaviour
+{
+    /// <summary>
+    /// Dialogue for this trigger behavior.
+    /// </summary>
+    public Dialogue dialogue;
+
+    /// <summary>
+    /// Start button.
+    /// </summary>
+    public Button button;
+
+    /// <summary>
+    /// Flag to keep track if button was click in order to start fade.
+    /// </summary>
+    public bool startFade = false;
+
+    /// <summary>
+    /// Update to check if we need to fade.
+    /// </summary>
+    void Update()
+    {
+        // If fade has started.
+        if (startFade)
+        {
+            // Compute the alpha value based on the button's image.
+            float newAlpha = button.image.color.a - Mathf.Clamp01(Time.deltaTime / 0.5f);
+            Debug.Log("New alpha value: " + newAlpha);
+
+            // Decrease alpha value.
+            // TODO This doesn't fade children components.
+            button.image.color = new Color(button.image.color.r, button.image.color.g, button.image.color.b, newAlpha);
+
+            // If the button is faded out.
+            if(newAlpha <= 0.0f)
+            {
+                // Stop the fade.
+                startFade = false;
+
+                // Destroy the button.
+                // TODO This doesn't destroy children components.
+                Destroy(button);
+
+                // Start first dialogue.
+                FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Triggered on click to start fade.
+    /// </summary>
+    public void TriggerFade()
+    {
+        Debug.Log("Triggering fade.");
+        startFade = true;
+    }
+}
