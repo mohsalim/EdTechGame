54c195a0231dff8a776992efa0bdb9ff9585c7e2
Mohammad Hassan Salim <mo_h_salim@hotmail.com>
2018-03-20 23:04:34 -0400

    Removed extra assetts. Tried to find mono file for debugging.

--- EdTechGame/Assets/RunCodeBehavior.cs
+++ EdTechGame/Assets/RunCodeBehavior.cs
@@ -1,4 +1,4 @@
-﻿//using Microsoft.CSharp;
+﻿using Microsoft.CSharp;
 using System.CodeDom.Compiler;
 using System;
 using System.IO;
@@ -6,7 +6,8 @@ using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
 using UnityEngine.UI;
-using Microsoft.CSharp;
+using System.Text;
+using System.Runtime.InteropServices;
 
 public class RunCodeBehavior : MonoBehaviour
 {
@@ -24,6 +25,12 @@ public class RunCodeBehavior : MonoBehaviour
     /// Prefix for output.
     /// </summary>
     private const string OUTPUT_PREFIX = "Output:\n";
+
+    /// <summary>
+    /// Is this a build run or an editor run? This will determine certain variables.
+    /// TODO Consider a config file.
+    /// </summary>
+    private const bool IS_BUILD = false;
 
     /// <summary>
     /// Attach button event during initialization.
@@ -44,8 +51,122 @@ public class RunCodeBehavior : MonoBehaviour
     {
         Debug.Log("Run Code button clicked. Input text: " + inputText.text);
 
+        // Debugging to figure out Mono.exe path that the code expects during runtime.
+        // This is useful to know during Editor runtime and Build runtime.
+        MonoToolsLocator();
+
         // Build and run code.
         BuildAndRunCode(inputText.text);
+    }
+
+    /// <summary>
+    /// Needed to check module paths. Original source summary is below:
+    /// 
+    /// Due to an issue with shadow copying  and app domains in mono, we cannot currently use 
+    /// Process.GetCurrentProcess ().MainModule.FileName (which would give the same result)
+    /// when running in an AppDomain (eg System.Web hosts).
+    ///
+    /// Using native Windows API to get current process filename. This will only
+    /// be called when running on Windows.
+    /// </summary>
+    /// <param name="hModule"></param>
+    /// <param name="lpFilename"></param>
+    /// <param name="nSize"></param>
+    /// <returns></returns>
+    [DllImport("kernel32.dll")]
+    static extern uint GetModuleFileName([In] IntPtr hModule, [Out] StringBuilder lpFilename, [In] int nSize);
+
+    /// <summary>
+    /// For debugging Mono production issue.
+    /// </summary>
+    private void MonoToolsLocator()
+    {
+        var ev = Environment.GetEnvironmentVariables();
+        Debug.Log("MONO_PATH = " + ev["MONO_PATH"]);
+        Debug.Log("MONO_GC_PARAMS = " + ev["MONO_GC_PARAMS"]);
+
+        string Mono = null;
+        string McsCSharpCompiler;
+        string VBCompiler;
+        string AssemblyLinker;
+        var gac = typeof(Environment).GetProperty("GacPath", BindingFlags.Static | BindingFlags.NonPublic);
+        var getGacMethod = gac.GetGetMethod(true);
+        var GacPath = Path.GetDirectoryName((string)getGacMethod.Invoke(null, null));
+        Debug.Log("GacPath = " + GacPath);
+
+        if (Path.DirectorySeparatorChar == '\\')
+        {
+            Debug.Log("GacPath reached if.");
+
+            StringBuilder moduleName = new StringBuilder(1024);
+            GetModuleFileName(IntPtr.Zero, moduleName, moduleName.Capacity);
+            string processExe = moduleName.ToString();
+            string fileName = Path.GetFileName(processExe);
+            if (fileName.StartsWith("mono") && fileName.EndsWith(".exe"))
+                Mono = processExe;
+
+            if (!File.Exists(Mono))
+                Mono = Path.Combine(
+                    Path.GetDirectoryName(
+                        Path.GetDirectoryName(GacPath)),
+                    "bin\\mono.exe");
+
+            if (!File.Exists(Mono))
+                Mono = Path.Combine(
+                    Path.GetDirectoryName(
+                        Path.GetDirectoryName(
+                            Path.GetDirectoryName(GacPath))),
+                    "mono\\mini\\mono.exe");
+
+            //if (!File.Exists (Mono))
+            //	throw new FileNotFoundException ("Windows mono path not found: " + Mono);
+
+            McsCSharpCompiler = Path.Combine(GacPath, "4.5", "mcs.exe");
+            if (!File.Exists(McsCSharpCompiler))
+            {
+                // Starting from mono\mcs\class
+                McsCSharpCompiler = Path.Combine(Path.GetDirectoryName(GacPath), "lib", "net_4_x", "mcs.exe");
+            }
+
+            //if (!File.Exists (CSharpCompiler))
+            //	throw new FileNotFoundException ("C# compiler not found at " + CSharpCompiler);
+
+            VBCompiler = Path.Combine(GacPath, "4.5\\vbnc.exe");
+            AssemblyLinker = Path.Combine(GacPath, "4.5\\al.exe");
+
+            if (!File.Exists(AssemblyLinker))
+            {
+                AssemblyLinker = Path.Combine(Path.GetDirectoryName(GacPath), "lib\\net_4_x\\al.exe");
+                //	if (!File.Exists (AssemblyLinker))
+                //		throw new FileNotFoundException ("Windows al path not found: " + AssemblyLinker);
+            }
+        }
+        else
+        {
+            Debug.Log("GacPath reached else.");
+
+            Mono = Path.Combine(GacPath, "bin", "mono");
+            if (!File.Exists(Mono))
+                Mono = "mono";
+
+            var mscorlibPath = new Uri(typeof(object).Assembly.CodeBase).LocalPath;
+            McsCSharpCompiler = Path.GetFullPath(Path.Combine(mscorlibPath, "..", "..", "..", "..", "bin", "mcs"));
+            if (!File.Exists(McsCSharpCompiler))
+                McsCSharpCompiler = "mcs";
+
+            VBCompiler = Path.GetFullPath(Path.Combine(mscorlibPath, "..", "..", "..", "..", "bin", "vbnc"));
+            if (!File.Exists(VBCompiler))
+                VBCompiler = "vbnc";
+
+            AssemblyLinker = Path.GetFullPath(Path.Combine(mscorlibPath, "..", "..", "..", "..", "bin", "al"));
+            if (!File.Exists(AssemblyLinker))
+                AssemblyLinker = "al";
+        }
+
+        Debug.Log("Mono = " + Mono);
+        Debug.Log("McsCSharpCompiler = " + McsCSharpCompiler);
+        Debug.Log("VBCompiler = " + VBCompiler);
+        Debug.Log("AssemblyLinker = " + AssemblyLinker);
     }
 
     /// <summary>
@@ -57,7 +178,9 @@ public class RunCodeBehavior : MonoBehaviour
     private int BuildAndRunCode(string code)
     {
         // Compiler the code.
-        string outputExe = AppDomain.CurrentDomain.BaseDirectory + "\\TestAsm.exe";
+        // NOTE: I need this for EXE build vs development build.
+        string outputExe = (IS_BUILD ? AppDomain.CurrentDomain.BaseDirectory + "\\" : "" ) + "TestAsm.exe";
+
         Debug.Log("Setting parameters in order to compile code to: " + outputExe);
         CompilerParameters parameters = new CompilerParameters()
         {
