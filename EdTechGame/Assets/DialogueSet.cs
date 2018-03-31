51c86b7a3e0cd480fc62d4ab929449e52f3a60bd
Mohammad Hassan Salim <mo_h_salim@hotmail.com>
2018-03-31 18:46:27 -0400

    Fixed spacing

--- EdTechGame/Assets/DialogueSet.cs
+++ EdTechGame/Assets/DialogueSet.cs
@@ -14,13 +14,13 @@ namespace Assets
             {
                 name = NpcNames.BROSEPH,
                 sentences = new string[]
-                            {
-                                "Bro, did you finish the programming homework?",
-                                "I can't figure it out! Can you please help me solve it?",
-                                "Come on bro! I'll pay you back big time time.",
-                                "Brooooooooooooooooooooooooooooooooooooooooooooo, please?!",
-                                "I think we have to print 'hello word' followed by 'goodbye world', but I'm not sure how to do that!"
-                            },
+                {
+                    "Bro, did you finish the programming homework?",
+                    "I can't figure it out! Can you please help me solve it?",
+                    "Come on bro! I'll pay you back big time time.",
+                    "Brooooooooooooooooooooooooooooooooooooooooooooo, please?!",
+                    "I think we have to print 'hello word' followed by 'goodbye world', but I'm not sure how to do that!"
+                },
                 successMessage = "Nice bro! I think that was right!",
                 sprite = NpcNames.BROSEPH_SPRITE_HAPPY
             };
@@ -36,10 +36,10 @@ namespace Assets
             {
                 name = NpcNames.BROSEPH,
                 sentences = new string[]
-                            {
-                                "The assignment says that we to change the variable 'number' to 8.",
-                                "But bro, it also says we got to do it before printing the variable."
-                            },
+                {
+                    "The assignment says that we to change the variable 'number' to 8.",
+                    "But bro, it also says we got to do it before printing the variable."
+                },
                 successMessage = "That seems right. You're so smart bro!",
                 sprite = NpcNames.BROSEPH_SPRITE_HAPPY
             };
